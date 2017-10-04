925eb7398c1414bf8e6113962b6748a0724e134b
jroberts3136 <Jroberts3136@gmail.com>
2017-10-04 08:48:59 -0400

    Initial commit

---
+++ src/com/company/BankAccount.java
@@ -0,0 +1,32 @@
+package com.company;
+
+/**
+ * Created by jroberts3136 on 10/4/17.
+ */
+public class BankAccount {
+    public String accountName;
+    public double accountBalance;
+
+    public BankAccount(String name, double balance){
+        accountName = name;
+        accountBalance = balance;
+    }
+    public void deposit(double deposit){
+        accountBalance += deposit;
+        System.out.println("Your balance is now $" + accountBalance + ".");
+    }
+    public void withdraw(double withdraw){
+        if (withdraw <= accountBalance){
+            accountBalance -= withdraw;
+            System.out.println("Your balance is now $" + accountBalance + ".");
+        }
+        else{
+            System.out.println("You cannot withdraw that much. Your balance is only $" + accountBalance + ".");
+        }
+    }
+    /*
+    public void pncWithdraw(double pncWithdraw){
+
+    }
+    */
+}
